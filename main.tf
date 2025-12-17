08c5d4d209ee63da673bed74947e1dc11ac40877
hashbury1 <efefiomarchibong@gmail.com.com>
2025-12-17 22:40:59 +0100

    updated nginz

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.26.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+
+
+
+
+
 ### Define the Network in 2 AZs
 resource "aws_vpc" "main" { cidr_block = "10.0.0.0/16" }
 
