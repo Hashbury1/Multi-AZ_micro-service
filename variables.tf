30e61cd1d7030760e75aeed221277e9a41b0dbaa
hashbury1 <efefiomarchibong@gmail.com.com>
2025-12-18 10:23:56 +0100

    added variable file

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "aws_account_id" {
+  description = "721912316979"
+  type        = string
+}
+
+variable "app_name" {
+  type    = string
+  default = "flask-event-monitor"
+}
